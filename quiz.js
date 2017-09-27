93b8c35bfe3ee4542108fe9d566707f64fd63b6a
Emily Chai <echai93@gmail.com>
2017-09-27 12:29:48 +0100

    Function to shuffle elements in an array

--- quiz.js
+++ quiz.js
@@ -79,4 +79,23 @@
               ],
     };
 
+    var display = [];
+    var numberOfQuestions = 30;
+
+    // Method to shuffle elements in an array
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+      // Whilst there are elements to shuffle
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array;
+    }
+
 })();
